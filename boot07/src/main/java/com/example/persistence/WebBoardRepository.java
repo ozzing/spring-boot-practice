58f4b0704f5ef050014fa5833ce2c650e48edc77
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-12 02:12:33 +0900

    ch7: reply REST

---
+++ boot07/src/main/java/com/example/persistence/WebBoardRepository.java
@@ -0,0 +1,35 @@
+package com.example.persistence;
+
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.domain.QWebBoard;
+import com.example.domain.WebBoard;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+
+public interface WebBoardRepository extends CrudRepository<WebBoard, Long>, QuerydslPredicateExecutor<WebBoard>{
+	
+	public default Predicate makePredicate(String type, String keyword) {
+		BooleanBuilder builder = new BooleanBuilder();
+		QWebBoard board = QWebBoard.webBoard;
+		builder.and(board.bno.gt(0));
+		
+		if(type == null) {
+			return builder;
+		}
+		
+		switch(type){
+		case "t":
+			builder.and(board.title.like("%"+keyword+"%"));
+			break;
+		case "c":
+			builder.and(board.content.like("%"+keyword+"%"));
+			break;
+		case "w":
+			builder.and(board.writer.like("%"+keyword+"%"));
+		}
+		
+		return builder;
+	}
+}
