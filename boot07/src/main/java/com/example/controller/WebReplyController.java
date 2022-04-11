4ea1c7ab46a8f5fd22dda4d761acd1478cf8ee7a
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-12 04:29:26 +0900

    ch7: reply Ajax

--- boot07/src/main/java/com/example/controller/WebReplyController.java
+++ boot07/src/main/java/com/example/controller/WebReplyController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -82,5 +83,17 @@ public class WebReplyController{
 		board.setBno(bno);
 		
 		return new ResponseEntity<>(getListByBoard(board), HttpStatus.CREATED);
+	}
+	
+	@Transactional
+	@GetMapping("/{bno}")
+	public ResponseEntity<List<WebReply>> getReplies(@PathVariable("bno")Long bno){
+		
+		log.info("get All replies...................");
+		
+		WebBoard board = new WebBoard();
+		board.setBno(bno);
+		
+		return new ResponseEntity<>(getListByBoard(board), HttpStatus.OK);
 	}
 }
