7a654fd5bf0e12438bb1a69b9b79cc1abd9c0a5e
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-11 19:20:16 +0900

    ch6: paging, search

--- boot06/src/main/java/com/example/controller/WebBoardController.java
+++ boot06/src/main/java/com/example/controller/WebBoardController.java
@@ -1,8 +1,20 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.WebBoard;
+import com.example.persistence.WebBoardRepository;
+import com.example.vo.PageMaker;
+import com.example.vo.PageVO;
 
 import lombok.extern.java.Log;
 
@@ -11,9 +23,21 @@ import lombok.extern.java.Log;
 @Log
 public class WebBoardController{
 	
-	@GetMapping("/list")
-	public void list() {
-		log.info("list() called...");
-	}
+	@Autowired
+	private WebBoardRepository repo;
 	
+	@GetMapping("/list")
+	public void list(@ModelAttribute("pageVO") PageVO vo, Model model) {
+		Pageable page = vo.makePageable(0, "bno");
+		
+		Page<WebBoard> result = repo.findAll(repo.makePredicate(vo.getType(), vo.getKeyword()), page);
+		
+		log.info("" + page);
+		log.info("" + result);
+		
+		log.info("TOTAL PAGE NUMBER: " + result.getTotalPages());
+		
+		model.addAttribute("result", new PageMaker(result));
+	}
+
 }
--- boot06/src/test/java/com/example/WebBoardRepositoryTests.java
+++ boot06/src/test/java/com/example/WebBoardRepositoryTests.java
@@ -6,14 +6,21 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.data.domain.Sort;
 
 import com.example.domain.WebBoard;
 import com.example.persistence.WebBoardRepository;
 
+import lombok.extern.java.Log;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Log
 @Commit
 public class WebBoardRepositoryTests{
 	
@@ -31,5 +38,27 @@ public class WebBoardRepositoryTests{
 			
 			repo.save(board);
 		});
+	}
+	
+	@Test
+	public void testList1() {
+		Pageable pageable = PageRequest.of(0, 20, Sort.Direction.DESC, "bno");
+		Page<WebBoard> result = repo.findAll(repo.makePredicate(null,null), pageable);
+		
+		log.info("PAGE: "+result.getPageable());
+		log.info("--------------------");
+		
+		result.getContent().forEach(board->log.info(""+board));
+	}
+	
+	@Test
+	public void testList2() {
+		Pageable pageable = PageRequest.of(0, 20, Sort.Direction.DESC, "bno");
+		Page<WebBoard> result = repo.findAll(repo.makePredicate("t","10"), pageable);
+		
+		log.info("PAGE: "+result.getPageable());
+		log.info("--------------------");
+		
+		result.getContent().forEach(board->log.info(""+board));
 	}
 }
