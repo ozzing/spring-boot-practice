b4ab0bec21fd5a465d08cc366810c0b5d5f1f4fb
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-13 04:48:15 +0900

    ch8: HttpSession login/logout

---
+++ boot08/src/main/java/com/example/security/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import lombok.extern.java.Log;
+
+@Log
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		log.info("security config....................");
+		
+		http.authorizeRequests().antMatchers("/guest/**").permitAll();
+		http.authorizeRequests().antMatchers("/manager/**").hasRole("MANAGER");
+		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
+		http.formLogin().loginPage("/login");
+		http.exceptionHandling().accessDeniedPage("/accessDenied");
+		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		
+		return new PasswordEncoder() {
+			@Override
+			public String encode(CharSequence rawPassword) {
+				return rawPassword.toString();
+			}
+			
+			@Override
+			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+				return rawPassword.equals(encodedPassword);
+			}
+		};
+	}
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+		log.info("build Auth global.......");
+		
+		auth.inMemoryAuthentication().withUser("manager").password("1111").roles("MANAGER");
+	}
+	
+}
