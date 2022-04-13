9ba1107b2cb9389e2aefba1e72c52498bfe35a80
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-13 22:14:52 +0900

    ch8: Custom UserDetailsService

--- boot08/src/main/java/com/example/security/SecurityConfig.java
+++ boot08/src/main/java/com/example/security/SecurityConfig.java
@@ -1,18 +1,18 @@
 package com.example.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import lombok.extern.java.Log;
 
 @Log
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	ExampleUsersService exampleUsersService;
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
@@ -24,29 +24,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.formLogin().loginPage("/login");
 		http.exceptionHandling().accessDeniedPage("/accessDenied");
 		http.logout().logoutUrl("/logout").invalidateHttpSession(true);
+		http.userDetailsService(exampleUsersService);
 	}
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		
-		return new PasswordEncoder() {
-			@Override
-			public String encode(CharSequence rawPassword) {
-				return rawPassword.toString();
-			}
-			
-			@Override
-			public boolean matches(CharSequence rawPassword, String encodedPassword) {
-				return rawPassword.equals(encodedPassword);
-			}
-		};
-	}
-	
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
-		log.info("build Auth global.......");
-		
-		auth.inMemoryAuthentication().withUser("manager").password("1111").roles("MANAGER");
-	}
+//	@Bean
+//	public PasswordEncoder passwordEncoder() {
+//		
+//		return new PasswordEncoder() {
+//			@Override
+//			public String encode(CharSequence rawPassword) {
+//				return rawPassword.toString();
+//			}
+//			
+//			@Override
+//			public boolean matches(CharSequence rawPassword, String encodedPassword) {
+//				return rawPassword.equals(encodedPassword);
+//			}
+//		};
+//	}
+//	
+//	@Autowired
+//	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+//		log.info("build Auth global.......");
+//		
+//		String query1 = "SELECT uid, username, CONCAT('{noop}', upw) password, true enabled FROM tbl_members WHERE uid = ?";
+//		String query2 = "SELECT member uid, role_name role FROM tbl_member_roles WHERE member = ?";
+//		
+//		auth.jdbcAuthentication()
+//		.dataSource(dataSource)
+//		.usersByUsernameQuery(query1)
+//		.rolePrefix("ROLE_")
+//		.authoritiesByUsernameQuery(query2);
+//	}
 	
 }
